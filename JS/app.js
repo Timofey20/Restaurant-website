62ade1015cea6d0e801ee747640c53aafcf7133b
Timofey <timofey20ab@gmail.com>
2020-11-28 21:48:16 +0700

    Add js

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,15 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() != 0) {
+        $("#toTop").fadeIn();
+      } else {
+        $("#toTop").fadeOut();
+      }
+    });
+
+    $("#toTop").click(function () {
+      $("body,html").animate({ scrollTop: 0 }, 800);
+    });
+  });
+});
